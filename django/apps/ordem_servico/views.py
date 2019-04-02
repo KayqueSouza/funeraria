e177657d71c1e6265d90ad68a4cc1b0ddebf9c99
Rafael Henter <henter.r@gmail.com>
2019-04-02 11:58:54 -0300

    Ajuste no form de cadastro

--- django/apps/ordem_servico/views.py
+++ django/apps/ordem_servico/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView, FormView
 
 from .models import OrdemServico
-#from .forms import OrdemServicoForm
 
 
 class OrdemServicoView(LoginRequiredMixin, ListView):
@@ -16,31 +15,42 @@ class OrdemServicoView(LoginRequiredMixin, ListView):
         context['page_title'] = 'Ordem de Serviço'
         return context
 
+
 class CopaView(LoginRequiredMixin, ListView):
     context_object_name = "ordem_servicos"
     queryset = OrdemServico.objects.all()
     template_name = "ordem_servico/listagem_copa.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['page_title'] = 'Copa'
         return context
 
 
-
-
 class OrdemServicoCreate(LoginRequiredMixin, CreateView):
     model = OrdemServico
-    fields = '__all__'
+    fields = (
+        'funeraria', 'falecido', 'cpf_falecido', 'especie', 'abrangencia', 'tipo',
+        'taxas_apagar', 'remocao', 'codigo', 'urna', 'biblia', 'cristo', 'sem_adereco',
+        'veu', 'preparacao', 'ornamentacao', 'obs_ornamentacao', 'cafe', 'local_velorio',
+        'local_sepultamento', 'previsao_velorio', 'data_sepultamento', 'inicio_remocao',
+        'termino_remocao', 'inicio_urna', 'termino_urna', 'inicio_ctp', 'termino_ctp',
+        'inicio_cto', 'termino_cto', 'inicio_exp', 'termino_exp', 'inicio_cafe', 'termino_cafe',
+        'inicio_flora', 'termino_flora'
+    )
     template_name = "ordem_servico/os_cadastro.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['page_title'] = 'Criar Ordem de Serviço'
         return context
 
+
 class AgentesView(LoginRequiredMixin, ListView):
     context_object_name = "ordem_servicos"
     queryset = OrdemServico.objects.all()
     template_name = "ordem_servico/agentes.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['page_title'] = 'Agentes'
